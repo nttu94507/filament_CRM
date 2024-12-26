5724a3db4d813e3dda71182778f6159a45e4266f
nttu94507 <nttu94507@gmail.com>
2024-12-26 11:06:34 +0800

    fix bug sql can not use 0

--- app/Filament/Resources/ShipmentResource.php
+++ app/Filament/Resources/ShipmentResource.php
@@ -5,11 +5,12 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\ShipmentResource\Pages;
 use App\Models\Probe;
 use App\Models\Shipment;
-use Filament\Actions\DeleteAction;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Forms\Set;
 use Filament\Resources\Resource;
+use Filament\Support\Colors\Color;
+use Filament\Support\Enums\FontWeight;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
@@ -86,6 +87,7 @@ class ShipmentResource extends Resource
                         Forms\Components\TextInput::make('note')
                             ->label('備註'),
                     ])
+                    ->inlineLabel()
                     ->columns('2'),
             ]);
     }
@@ -96,40 +98,116 @@ class ShipmentResource extends Resource
             ->columns([
                 //
                 Tables\Columns\TextColumn::make('action_type')
-                    ->label('出貨方式')
+                    ->label('類型')
                     ->formatStateUsing(fn(string $state): string => match ($state) {
-                        '0' => '出貨',
-                        '1' => '換貨',
-                        '2' => '借測',
-                        '3' => '退貨',
-
+                        '1' => '出貨',
+                        '2' => '換貨',
+                        '3' => '借測',
+                        '4' => '退貨',
                     })
                     ->color(fn(string $state): string => match ($state) {
-                        '0' => 'success',
+                        '1' => 'success',
+                        '2' => 'warning',
+                        '3' => 'gray',
+                        '4' => 'danger',
+                    })
+                    ->weight(FontWeight::ExtraBold)
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('status')
+                    ->label('狀態')
+                    ->formatStateUsing(fn(string $state): string => match ($state) {
+                        '1' => '進行中',
+                        '2' => '已完成',
+                        '3' => '已取消',
+                    })
+                    ->color(fn(string $state): string => match ($state) {
                         '1' => 'warning',
-                        '2' => 'gray',
+                        '2' => 'success',
                         '3' => 'danger',
-                    }),
+                    })
+                    ->searchable(),
                 Tables\Columns\TextColumn::make('case_id')
-                ->label('出貨單號'),
-                Tables\Columns\TextColumn::make('customer.company_name'),
+                    ->label('出貨單號')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('customer.company_name')
+                    ->searchable(),
                 Tables\Columns\TextColumn::make('shipment_items_count')
                     ->label('probe 數量')
                     ->counts('shipment_items'),
                 Tables\Columns\TextColumn::make('total')
-                    ->label('總成本'),
+                    ->label('總成本')
+                    ->searchable(),
                 Tables\Columns\TextColumn::make('note')
-                    ->label('備註'),
+                    ->label('備註')
+                    ->searchable(),
 
             ])
             ->filters([
-                //
-            ])
+                Tables\Filters\Filter::make('test')
+                    ->form([
+                                Forms\Components\TextInput::make('case_id')
+                                    ->label('出貨單號')
+                                    ->columnSpan(1)
+                                    ->inlineLabel(),
+                                Forms\Components\Select::make('status')
+                                    ->label('狀態')
+                                    ->options([
+                                        '1' => '進行中',
+                                        '2' => '已完成',
+                                        '3' => '已取消',
+                                    ])
+                                ->columnSpan(1)
+                                    ->inlineLabel()
+
+                    ])
+                    ->columns(2)
+                    ->columnSpanFull()
+                ->query(function (Builder $query,array $data): Builder {
+                    return $query
+                        ->when(
+                            $data['case_id'],
+                            fn (Builder $query, $data): Builder => $query->where('case_id', 'like','%'.$data.'%'),
+                        )
+                        ->when(
+                            $data['status'],
+                            fn (Builder $query, $data): Builder => $query->where('status', '=',$data),
+                        )
+                        ;
+                })
+            ],
+                layout: Tables\Enums\FiltersLayout::AboveContent
+            )
+//            ->filtersFormSchema(fn (array $filters): array => [
+//                Forms\Components\Section::make('Visibility')
+//                    ->description('These filters affect the visibility of the records in the table.')
+//                    ->schema([
+//                        $filters['test'],
+////                        $filters['published_at'],
+//                    ])
+//                    ->columns(2)
+//                    ->columnSpanFull(),
+////                $filters['author'],
+//            ])
             ->recordUrl(false)
             ->actions([
-//                Tables\Actions\EditAction::make()
-//                ->hidden(fn($record) => !$record->is_editable),
-            Tables\Actions\DeleteAction::make('delete')
+                Tables\Actions\ViewAction::make('view')
+                ->button(),
+                //                    ->color('warning'),
+                Tables\Actions\Action::make('completed')
+                    ->label('完成')
+                    ->color(Color::Emerald)
+                    ->action(fn(Shipment $record) => $record->update(['status' => 1]))
+                    ->requiresConfirmation()
+                    ->button()
+                    ->disabled(fn(Shipment $record) => $record->status !== 0 ? true : false),
+                Tables\Actions\Action::make('delete')
+                    ->label('取消')
+                    ->color('danger')
+                    ->button()
+                    ->action(fn(Shipment $record) => $record->update(['status' => 2]))
+                    ->requiresConfirmation()
+                    ->disabled(fn(Shipment $record) => $record->status !== 0 ? true : false),
 
             ])
             ->bulkActions([
@@ -152,6 +230,7 @@ class ShipmentResource extends Resource
             'index' => Pages\ListShipments::route('/'),
             'create' => Pages\CreateShipment::route('/create'),
             'edit' => Pages\EditShipment::route('/{record}/edit'),
+            'view' => Pages\ViewShipment::route('/{record}'),
         ];
     }
 }
